00f1b9135be9524b0b3ab9932659c338098a5b7d
Kevin-Lof <kevinloffredi98@gmail.com>
2024-04-15 15:16:06 +0200

    eserciziGiorno1

---
+++ src/Esercizio1Giorno1/Esercizio1.java
@@ -0,0 +1,42 @@
+package Esercizio1Giorno1;
+
+import java.util.Arrays;
+
+public class Esercizio1 {public static void main(String[] args) {
+    int a = 20;
+    int b = 50;
+    int result = multiply(a, b);
+    System.out.println(result);
+
+    System.out.println(concat("bla", 6));
+
+    String[] arr = insertIntoArray(new String[]{"A", "B", "C", "D", "E"}, "aaaaa");
+
+    System.out.println(Arrays.toString(arr));
+
+
+}
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static String concat(String str, int num) {
+        return str + num;
+    }
+
+    public static String[] insertIntoArray(String[] stringArray, String str) {
+        String[] array = new String[6];
+        for (int i = 0; i < stringArray.length +1 ; i++) {
+            if(i < 2) {
+                array[i] = stringArray[i];
+            } else if(i == 2) {
+                array[i] = str;
+            } else {
+                array[i] = stringArray[i - 1];
+            }
+        }
+
+        return array;
+    }
+}
